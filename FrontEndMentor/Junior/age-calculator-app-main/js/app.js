d1cb6f8998b6bc5c099d33860c2a6ff4c831132d
Jeison Dias <jeison.dias@outlook.com>
2023-04-21 14:07:39 -0300

    Add functions to calculate the full date

--- FrontEndMentor/Junior/age-calculator-app-main/js/app.js
+++ FrontEndMentor/Junior/age-calculator-app-main/js/app.js
@@ -1,4 +1,4 @@
-//Form Fields
+// Form Fields
 
 const dayInput = document.querySelector('#day');
 const monthInput = document.querySelector('#month');
@@ -10,58 +10,83 @@ const cDay = today.getDate();
 const cMonth = today.getMonth() + 1;
 const cYear = today.getFullYear();
 
+// Result Fields
+
+const yearsText = document.querySelector('.years');
+const monthsText = document.querySelector('.months');
+const daysText = document.querySelector('.days');
+
+/*
 // Listeners
-dayInput.addEventListener('keyup', getFormDay);
-monthInput.addEventListener('keyup', getFormMonth);
-yearInput.addEventListener('keyup', getFormYear);
+dayInput.addEventListener('keyup', );
+monthInput.addEventListener('keyup',);
+yearInput.addEventListener('keyup');
+*/
+
+// Form Default Dates
+let dayInputData = 0;
+let monthInputData = 0;
+let yearInputData = 0;
 
 
-//Get the Date from the form and check if they're correct
 
-function getFormDay(e) {
-    e.preventDefault();
-    let day = dayInput.value;
-    checkDay(day);
-}
-
-function getFormMonth(e) {
-    e.preventDefault();
-    let month = monthInput.value;
-    checkMonth(month);
-}
-
-function getFormYear(e) {
-    e.preventDefault();
-    let year = yearInput.value;
-    checkYear(year);
-
-}
 
 // Check if the date is correct
 function checkDay(day) {
+    parseInt(day);
     if(day <= 0 || day > 31) {
         console.log('This is not a valid date')
     } else {
-        console.log(day + 'certo');
-        return day;
+        return parseInt(day);
     }
 }
 
 
 function checkMonth(month) {
+    parseInt(month);
     if(month <= 0 || month > 12) {
         console.log('This is not a valid Month')
     } else {
-        console.log(month + 'certo');
-        return month;
+        return parseInt(month);
     }
 }
 
 function checkYear(year) {
-    if(year < 0 || year > cYear) {
+    parseInt(year);
+    if(year <= 0 || year > cYear) {
         console.log('This is not a valid Year')
     } else {
-        console.log(year+ 'certo');
-        return year;
+        return parseInt(year);
+    }
+}
+
+
+function getYMD() {
+
+    dayInputData = checkDay(dayInput.value);
+    monthInputData = checkMonth(monthInput.value);
+    yearInputData = checkYear(yearInput.value);
+
+    
+    if (dayInputData > 0 && monthInputData > 0 && parseInt(yearInputData) < cYear && parseInt(yearInputData) > 0) {
+        let years = negativeDate(cYear - yearInputData);
+        yearsText.innerHTML = years;
+
+        let months = negativeDate((cMonth - parseInt(monthInputData)));
+        monthsText.innerHTML = months;
+
+        let days = negativeDate((cDay - parseInt(dayInputData)));
+        daysText.innerHTML = days;
+
+    } else {
+        console.log('Tem Coisa errada!')
+    }
+}
+
+function negativeDate(date) {
+    if(date < -1) {
+        return date * -1;
+    } else {
+        return date;
     }
 }
