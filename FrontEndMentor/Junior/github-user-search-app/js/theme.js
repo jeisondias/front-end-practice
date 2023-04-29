6106ec0a96a66c4dcf8e313ae34a89cc70cf21ed
Jeison Dias <jeison.dias@outlook.com>
2023-04-29 13:41:37 -0300

    Add the JS to switch themes

--- FrontEndMentor/Junior/github-user-search-app/js/theme.js
+++ FrontEndMentor/Junior/github-user-search-app/js/theme.js
@@ -1,22 +1,46 @@
 // Theme JS
 
-const buttonTheme = document.querySelector('.btnSwitch');
-
-console.log(buttonTheme)
+const themeBtn = document.querySelector('.btnSwitch');
 
 function getCurrentTheme() {
-    let theme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
+    // This checks the system's default theme. 
+    // If the theme scheme is dark, will set it to dark if not, it'll be light
 
-    console.log(theme)
+    let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+    localStorage.getItem('site.theme') ? theme = localStorage.getItem('site.theme'): null;
+
     return theme;
 }
 
 function loadTheme(theme) {
     const root = document.querySelector(':root');
-    console.log(root);
-    if (theme === "light") {
-        root.setAttribute('color-scheme', 'dark');
+
+    if (theme === 'light') {
+        themeBtn.innerHTML = 
+        `<span>DARK</span>
+        <img src="/assets/icon-moon.svg" alt="Dark">`
     } else {
-        root.setAttribute('color-scheme', 'light');
+        themeBtn.innerHTML = 
+        `<span>LIGHT</span>
+        <img src="/assets/icon-sun.svg" alt="Light">`
     }
+
+    root.setAttribute('color-scheme', `${theme}`);
 }
+
+themeBtn.addEventListener('click', () => {
+    let theme = getCurrentTheme();
+    if (theme === 'dark') {
+        theme = 'light';
+    } else {
+        theme = 'dark';
+    }
+    localStorage.setItem('site.theme', `${theme}`);
+    loadTheme(theme);
+
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadTheme(getCurrentTheme());
+})
