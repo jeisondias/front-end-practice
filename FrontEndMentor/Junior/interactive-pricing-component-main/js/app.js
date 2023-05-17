3a298f883700e984d526049b441fe885614232f2
Jeison Dias <jeison.dias@outlook.com>
2023-05-17 00:00:47 -0300

    Add script to change the value when the scroller is moved and when the discount is active

---
+++ FrontEndMentor/Junior/interactive-pricing-component-main/js/app.js
@@ -0,0 +1,50 @@
+const scroller = document.querySelector('#price');
+const pageViews = document.querySelector('#pageviews');
+const priceValue = document.querySelector('#price-value');
+const discountBtn = document.querySelector('#discountBtn');
+
+const price = [10, 12, 16, 24, 36];
+let price2 = [10, 12, 16, 24, 36];
+
+
+function showNewValue() {
+    
+    switch(scroller.value) {
+        case '1': 
+        pageViews.innerText = '10k Pageviews';
+        priceValue.innerText = `$${price2[0].toFixed(2)}`;
+        break;
+        case '2': 
+        pageViews.innerText = '50k Pageviews';
+        priceValue.innerText = `$${price2[1].toFixed(2)}`;
+        break;
+        case '3': 
+        pageViews.innerText = '100k Pageviews';
+        priceValue.innerText = `$${price2[2].toFixed(2)}`;
+        break;
+        case '4': 
+        pageViews.innerText = '500k Pageviews';
+        priceValue.innerText = `$${price2[3].toFixed(2)}`;
+        break;
+        case '5': 
+        pageViews.innerText = '1M Pageviews';
+        priceValue.innerText = `$${price2[4].toFixed(2)}`;
+        break;
+        default:
+            console.log('Not a valid value');
+    }
+}
+
+function discount() {
+    if (discountBtn.checked) {
+        price2 = price.map(i => i - (i * 0.25));
+        discountBtn.checked == true;
+        showNewValue()
+    } else {
+        price2 = price;
+        showNewValue()
+    }
+}
+
+discountBtn.addEventListener('click', discount);
+scroller.addEventListener('change', showNewValue);
